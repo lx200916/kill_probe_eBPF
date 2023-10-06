b9037fcd3114d4432ac3e548e8d7b02a50a8f7b6
Lee <44310445+lx200916@users.noreply.github.com>
2023-10-06 17:15:17 +0800

    Chaos: Cleanup Codes.

--- kill-probe/src/data_map.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
-
-use kill_probe_common::Data;
-#[derive(Debug, Clone,Default)]
-pub struct DataMap {
-    pub data: Arc<RwLock<HashMap<u64, Data>>>,
-}
